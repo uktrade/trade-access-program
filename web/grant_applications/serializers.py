76fe505c0a32ee2aeeaac7b8d8175bbf1fbd22fe
sean.blumenfeld <seanbl@me.com>
2020-08-25 12:53:12 +0100

    Add applications under review count to AboutYourBusiness screen

--- web/grant_applications/serializers.py
+++ web/grant_applications/serializers.py
@@ -8,10 +8,17 @@ class AboutYourBusinessTableSerializer(serializers.ModelSerializer):
     duns_number = serializers.CharField(label='Dun and Bradstreet Number')
     name = serializers.CharField(label='Company Name')
     previous_applications = serializers.SerializerMethodField(label='Previous Applications')
+    applications_in_review = serializers.SerializerMethodField(label='Applications in Review')
 
     class Meta:
         model = Company
-        fields = ['duns_number', 'name', 'previous_applications']
+        fields = ['duns_number', 'name', 'previous_applications', 'applications_in_review']
+
+    def get_applications_in_review(self, company):
+        return company.grantapplication_set.filter(
+            grant_application_process__isnull=False,
+            grant_application_process__decision__isnull=True
+        ).count()
 
     def get_previous_applications(self, company):
         return company.grantapplication_set.filter(
