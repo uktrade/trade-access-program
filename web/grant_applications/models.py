d4d4229fee1e5c2a69a36523059a71393c9d3db3
sean.blumenfeld <seanbl@me.com>
2020-08-27 11:47:34 +0100

    [TRAD-1] Change number-of-employees form field to choice/select field of 4 choices
     - Fewer than 10 employees
     - 10 to 49 employees
     - 50 to 249 employees
     - 250 or more employees

--- web/grant_applications/models.py
+++ web/grant_applications/models.py
@@ -2,12 +2,20 @@ from django.contrib.postgres.fields import JSONField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models import PROTECT
+from django.utils.translation import gettext_lazy as _
 
 from web.core.abstract_models import BaseMetaModel
 from web.grant_management.flows import GrantManagementFlow
 
 
 class GrantApplication(BaseMetaModel):
+
+    class NumberOfEmployees(models.TextChoices):
+        HAS_FEWER_THAN_10 = 'fewer-than-10', _('Fewer than 10')
+        HAS_10_TO_49 = '10-to-49', _('10 to 49')
+        HAS_50_TO_249 = '50-to-249', _('50 to 249')
+        HAS_250_OR_MORE = '250-or-more', _('250 or More')
+
     search_term = models.CharField(max_length=500)
     company = models.ForeignKey('companies.Company', on_delete=PROTECT, null=True)
     applicant_full_name = models.CharField(null=True, max_length=500)
@@ -26,7 +34,9 @@ class GrantApplication(BaseMetaModel):
     goods_and_services_description = models.TextField(null=True)
     business_name_at_exhibit = models.CharField(null=True, max_length=500)
     turnover = models.IntegerField(null=True, validators=[MinValueValidator(0)])
-    number_of_employees = models.IntegerField(null=True, validators=[MinValueValidator(1)])
+    number_of_employees = models.CharField(
+        null=True, choices=NumberOfEmployees.choices, max_length=20
+    )
     sector = models.CharField(null=True, max_length=500)
     website = models.URLField(null=True, max_length=500)
     has_exported_before = models.BooleanField(null=True)
