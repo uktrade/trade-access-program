d8bdfd6056e59eab7ce2252d14172600dd78d486
erdem <erdem@prezola.com>
2021-03-04 12:43:59 +0000

    linter warnings clean up for backoffice

--- backoffice/web/grant_applications/api_urls.py
+++ backoffice/web/grant_applications/api_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from web.grant_applications.apis import GrantApplicationsViewSet, StateAidViewSet, SendApplicationResumeEmailView
+from web.grant_applications.apis import (
+    GrantApplicationsViewSet, StateAidViewSet, SendApplicationResumeEmailView
+)
 
 router = SimpleRouter()
 router.register('grant-applications', GrantApplicationsViewSet, basename='grant-applications')
@@ -9,5 +11,9 @@ router.register('state-aid', StateAidViewSet, basename='state-aid')
 
 app_name = 'grant-applications'
 urlpatterns = [
-    path('send-resume-application-email/', SendApplicationResumeEmailView.as_view(), name='send-application-email'),
+    path(
+        'send-resume-application-email/',
+        SendApplicationResumeEmailView.as_view(),
+        name='send-application-email'
+    ),
 ] + router.urls
