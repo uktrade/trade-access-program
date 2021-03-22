f3aefd8c3d03859a88ec2dc5a7c3600d4b6d4f30
erdmoz <erdmoz@erdemozkol.com>
2021-03-22 14:56:55 +0000

    updated backoffice settings

--- backoffice/config/settings/ci.py
+++ backoffice/config/settings/ci.py
@@ -6,6 +6,7 @@ DNB_SERVICE_TOKEN = 'a-fake-token'
 COMPANIES_HOUSE_URL = 'http://test.com'
 COMPANIES_HOUSE_COMPANIES_URL = 'http://test.com'
 COMPANIES_HOUSE_API_KEY = 'a-fake-key'
+NOTIFY_API_KEY = 'test-api-key'
 
 INSTALLED_APPS += [
     'django_extensions',
